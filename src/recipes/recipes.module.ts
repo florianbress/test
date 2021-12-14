1049e5d1e81011c93e629714fa79933b77b0f545
jlorezz <florianbress@gmail.com>
2021-12-15 00:04:30 +0100

    implementation of ogma logger for service (recipes)

--- src/recipes/recipes.module.ts
+++ src/recipes/recipes.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { DateScalar } from '../common/scalars/date.scalar';
-import { RecipesResolver } from './recipes.resolver';
-import { RecipesService } from './recipes.service';
+import { Module } from "@nestjs/common";
+import { OgmaModule } from "@ogma/nestjs-module";
+import { DateScalar } from "../common/scalars/date.scalar";
+import { RecipesResolver } from "./recipes.resolver";
+import { RecipesService } from "./recipes.service";
 
 @Module({
   providers: [RecipesResolver, RecipesService, DateScalar],
+  imports: [OgmaModule.forFeature(RecipesService.name)],
 })
 export class RecipesModule {}
--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { OgmaLogger, OgmaService } from "@ogma/nestjs-module";
 import { NewRecipeInput } from "./dto/new-recipe.input";
 import { RecipesArgs } from "./dto/recipes.args";
 import { Recipe } from "./models/recipe.model";
 
 @Injectable()
 export class RecipesService {
+  private readonly recipes: Recipe[] = [];
+
+  constructor(
+    @OgmaLogger(RecipesService) private readonly logger: OgmaService
+  ) {}
+
   async create(data: NewRecipeInput): Promise<Recipe> {
     return {} as any;
   }
@@ -14,7 +21,11 @@ export class RecipesService {
   }
 
   async findAll(recipesArgs: RecipesArgs): Promise<Recipe[]> {
-    return [] as Recipe[];
+    this.logger.silly("Silly!");
+    this.logger.error("Error!");
+    this.logger.verbose("Verbose!");
+    this.logger.fatal("Fatal!");
+    return this.recipes;
   }
 
   async remove(id: string): Promise<boolean> {
